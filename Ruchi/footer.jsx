5125ea663e8eecfded261f8c091fcda077609dbd
Ruchi Barne <146323148+ruchibarne@users.noreply.github.com>
2025-10-15 22:44:25 +0530

    Add files via upload

---
+++ Ruchi/footer.jsx
@@ -0,0 +1,7 @@
+export default function footer(){
+    return (
+        <div className="text-dark text-center m-0 sticky-end mt-4" style={{backgroundColor: "#8FBC8B", height: "90px",width: "100%", textAlign: "center", padding: "30px"}}>
+            <b className="mb-0">&copy; 2025 Kerala Tourism. All rights reserved.</b>
+        </div>
+    );
+}
